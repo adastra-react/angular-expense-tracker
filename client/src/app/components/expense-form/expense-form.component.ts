e12ae61ffaadd3444be263dd107b02e3d27d6bd3
suneil <kxnglish@gmail.com>
2023-07-05 19:38:08 -0500

    form validation and error handling added

--- client/src/app/components/expense-form/expense-form.component.ts
+++ client/src/app/components/expense-form/expense-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Expense } from '../../services/expense.service';
+import * as Toastify from 'toastify-js'; // Import as namespace
 
 @Component({
   selector: 'app-expense-form',
@@ -13,10 +14,66 @@ export class ExpenseFormComponent {
   name = ''; // Variable to store the name of the expense
   amount = ''; // Variable to store the amount of the expense
 
-   // Function to add an expense
+  // Function to add an expense
   addExpense(): void {
+    // Additional validation logic
+    const nameContainsNumber = /\d/.test(this.name); // Check if the name contains a digit
+    const amountContainsLetter = /[a-zA-Z]/.test(this.amount); // Check if the amount contains a letter
+
+    if (nameContainsNumber) {
+            Toastify({
+            text: 'Name should not contain numbers.',
+            className: 'toastify-error',
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            gravity: "top", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "#FFBF00",
+        },
+      }).showToast();
+      return; // Exit the function if the name contains a number
+    }
+
+    if (amountContainsLetter) {
+      Toastify({
+        text: 'Amount should not contain letters.',
+        className: 'toastify-error',
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "#FFBF00",
+    },
+  }).showToast();
+      return; // Exit the function if the amount contains a letter
+    }
+
+    if (!this.name || !this.amount) {
+      Toastify({
+        text: 'Name and amount are required.',
+        className: 'toastify-error',
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "#FFBF00",
+    },
+  }).showToast();
+      return; // Exit the function if name or amount is empty
+    }
+
+    // If all validations pass, create the expense and emit the expenseAdded event
     const expense: Expense = { name: this.name, amount: parseFloat(this.amount) };
-    this.expenseAdded.emit(expense); // Emit the expenseAdded event with the new expense
+    this.expenseAdded.emit(expense);
     this.name = ''; // Reset the name field
     this.amount = ''; // Reset the amount field
   }
