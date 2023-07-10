5c2274f75cfda3c779090f7fcecbcbd13f11d5e9
suneil <kxnglish@gmail.com>
2023-07-10 07:01:39 -0500

    new tests added

--- server/src/payoutService.js
+++ server/src/payoutService.js
@@ -1,29 +1,42 @@
 function calculatePayouts(expenses) {
+    // Calculate the total amount of expenses
     const total = expenses.reduce((total, expense) => total + expense.amount, 0);
-    const equalShare = total / expenses.length;
-
+  
+    // Calculate the equal share amount per person
+    const equalShare = expenses.length > 0 ? total / expenses.length : 0;
+  
+    // Initialize an array to store the payouts
     let payouts = [];
-
+  
     // Calculate who owes money and how much
     for (let i = 0; i < expenses.length; i++) {
-        if (expenses[i].amount < equalShare) {
-            for (let j = 0; j < expenses.length; j++) {
-                if (expenses[j].amount > equalShare) {
-                    let amountOwed = Math.min(equalShare - expenses[i].amount, expenses[j].amount - equalShare);
-                    expenses[i].amount += amountOwed;
-                    expenses[j].amount -= amountOwed;
-                    payouts.push({ owes: expenses[i].name, owed: expenses[j].name, amount: amountOwed });
-                    if (expenses[i].amount >= equalShare) {
-                        break;
-                    }
-                }
+      if (expenses[i].amount < equalShare) {
+        for (let j = 0; j < expenses.length; j++) {
+          if (expenses[j].amount > equalShare) {
+            // Calculate the amount owed between two persons
+            let amountOwed = Math.min(equalShare - expenses[i].amount, expenses[j].amount - equalShare);
+  
+            // Adjust the expense amounts accordingly
+            expenses[i].amount += amountOwed;
+            expenses[j].amount -= amountOwed;
+  
+            // Add the payout details to the payouts array
+            payouts.push({ owes: expenses[i].name, owed: expenses[j].name, amount: amountOwed });
+  
+            // Check if the owed amount is covered by the owing person's expenses
+            if (expenses[i].amount >= equalShare) {
+              break; // No need to continue calculating payouts for the current owing person
             }
+          }
         }
+      }
     }
-
+  
+    // Return the calculated total, equal share, and payouts
     return { total, equalShare, payouts };
-}
-
-module.exports = {
+  }
+  
+  module.exports = {
     calculatePayouts
-};
+  };
+  
--- server/src/tests/payoutService.spec.ts
+++ server/src/tests/payoutService.spec.ts
@@ -2,14 +2,61 @@ import { calculatePayouts } from '../payoutService';
 
 describe('PayoutService', () => {
   it('should calculate payouts correctly', () => {
+    // Define the test data
     const expenses = [
       { name: "Adriana", amount: 5.75 },
       { name: "Adriana", amount: 5.75 },
       { name: "Bao", amount: 12 }
     ];
+
+    // Call the function under test
     const result = calculatePayouts(expenses);
-    expect(result.total).toBe(23.5);
-    expect(result.equalShare).toBe(7.833333333333333);
-    expect(result.payouts.length).toBe(2);
+
+    // Assert the expected results
+    expect(result.total).toBe(23.5); // Check the total amount
+    expect(result.equalShare).toBeCloseTo(7.83); // Use toBeCloseTo for floating-point comparisons
+    expect(result.payouts.length).toBe(2); // Check the number of payouts
+  });
+
+  it('should return empty payouts if expenses array is empty', () => {
+    // Define the test data
+    const expenses = [];
+
+    // Call the function under test
+    const result = calculatePayouts(expenses);
+
+    // Assert the expected results
+    expect(result.total).toBe(0); // Check that the total amount is 0
+    expect(result.equalShare).toBe(0); // Check that the equal share is 0
+    expect(result.payouts.length).toBe(0); // Check that there are no payouts
+  });
+
+  it('should handle single expense correctly', () => {
+    // Define the test data
+    const expenses = [{ name: "Adriana", amount: 10 }];
+
+    // Call the function under test
+    const result = calculatePayouts(expenses);
+
+    // Assert the expected results
+    expect(result.total).toBe(10); // Check the total amount
+    expect(result.equalShare).toBe(10); // Check that the equal share is equal to the expense amount
+    expect(result.payouts.length).toBe(0); // Check that there are no payouts
+  });
+
+  it('should handle equal shares without any payouts', () => {
+    // Define the test data
+    const expenses = [
+      { name: "Adriana", amount: 5 },
+      { name: "Bao", amount: 5 }
+    ];
+
+    // Call the function under test
+    const result = calculatePayouts(expenses);
+
+    // Assert the expected results
+    expect(result.total).toBe(10); // Check the total amount
+    expect(result.equalShare).toBe(5); // Check the equal share
+    expect(result.payouts.length).toBe(0); // Check that there are no payouts
   });
 });
